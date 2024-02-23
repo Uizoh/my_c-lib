76db368a893f34356c927242416f5c0655df53c8
Uizoh <uizoh@proton.me>
2024-02-23 23:38:21 +0600

    Add test.c

--- my_vec.c
+++ my_vec.c
@@ -1,15 +1,31 @@
 #include "my_defaults.h"
 #include <stdlib.h>
 
+typedef enum TypeVec { INT, UINT, FLOAT } TypeVec;
+
 typedef struct Vec {
     u32 len;
     u32 capacity;
-    i32* arr;
+    TypeVec type;
+    union {
+        i32* _arr_i;
+        u32* _arr_u;
+        f32* _arr_f;
+    }
 }* Vec;
 
-Vec vec_new(u32 init_cap) {
+Vec vec_new(TypeVec type, u32 init_cap) {
     Vec temp = malloc(sizeof(struct Vec));
-    temp->arr = malloc(sizeof(i32) * init_cap);
+    temp->type = type;
+
+    switch (type) {
+    case INT:
+        temp->_arr_i = malloc(sizeof(i32) * init_cap);
+    case UINT:
+        temp->_arr_u = malloc(sizeof(u32) * init_cap);
+    case FLOAT:
+        temp->_arr_f = malloc(sizeof(f32) * init_cap);
+    }
 
     temp->len = 0;
     temp->capacity = init_cap;
@@ -17,13 +33,21 @@ Vec vec_new(u32 init_cap) {
     return temp;
 }
 
-void vec_append(Vec self, i32 item) {
+void vec_append(Vec self, void* item) {
     if (self->len >= self->capacity) {
         self->capacity += 50;
-        self->arr = realloc(self->arr, sizeof(i32) * self->capacity);
+
+        switch (self->type) {
+        case INT:
+            self->_arr_i = realloc(self->_arr_i, sizeof(i32) * self->capacity);
+        case UINT:
+            self->_arr_u = realloc(self->_arr_u, sizeof(u32) * self->capacity);
+        case FLOAT:
+            self->_arr_f = realloc(self->_arr_f, sizeof(f32) * self->capacity);
+        }
     }
 
-    self->arr[self->len] = (i32)item;
+    self->arr[self->len] = *(i32*)item;
     self->len += 1;
 }
 
