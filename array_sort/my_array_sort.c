cca6ecba72651006e797982e2ac9c65c411e9134
Uizoh <107351887+Uizoh@users.noreply.github.com>
2026-02-02 21:37:43 +0600

    Add Merge Sort implementation

--- array_sort/my_array_sort.c
+++ array_sort/my_array_sort.c
@@ -53,11 +53,64 @@ void insertionSort(int32_t* arr, uint32_t len) {
 
 
 // Merge sort algorithm implementation
-void merge(int32_t* arr, uint32_t len) {
+void merge(int32_t* left_array, int32_t* right_array, int32_t* arr, uint32_t len) {
+    uint32_t left_len = len / 2;
+    uint32_t right_len = len - left_len;
 
+    uint32_t o = 0, l = 0, r = 0; // Indices for original, left and right array
+
+    // Comparing and merging in sorted order
+    while (l < left_len && r < right_len) {
+        if (left_array[l] < right_array[r]) {
+            arr[o] = left_array[l];
+            o++;
+            l++;
+        } else {
+            arr[o] = right_array[r];
+            o++;
+            r++;
+        }
+    }
+
+    // When no elements from the right array is left to be compared
+    while (l < left_len) {
+        arr[o] = left_array[l];
+        o++;
+        l++;
+    }
+
+    // When no elements from the left array is left to be compared
+    while (r < right_len) {
+        arr[o] = right_array[r];
+        o++;
+        r++;
+    }
 }
 
 
 void mergeSort(int32_t* arr, uint32_t len) {
+    // Base case
+    if (len <= 1)
+        return;
 
+    uint32_t mid = len / 2;
+
+    int32_t left_array[mid];
+    int32_t right_array[len - mid];
+
+    // Inserting the elements to the left and right arrays
+    for (uint32_t i = 0, j = 0; i < len; i++) {
+        if (i < mid) {
+            left_array[i] = arr[i];
+        } else {
+            right_array[j] = arr[i];
+            j++;
+        }
+    }
+
+    // Recursive calls
+    mergeSort(left_array, mid);
+    mergeSort(right_array, len - mid);
+    merge(left_array, right_array, arr, len);
 }
+
--- array_sort/my_array_sort.h
+++ array_sort/my_array_sort.h
@@ -4,11 +4,14 @@
 #include "my_defaults.h"
 
 
-// An in-place array sorting algorightm that takes the array, the starting and ending index as parameters.
+// An in-place array sorting algorithm that takes the array, the starting and ending index as parameters
 void quickSort(int32_t* arr, int32_t start, int32_t end);
 
 // In-place array sorting algorithm
 void insertionSort(int32_t* arr, uint32_t len);
+
+// A recursive divide and conquer sorting algorithm that breaks down the array and builds them up in a sorted order
+void mergeSort(int32_t* arr, uint32_t len);
 
 
 #endif
